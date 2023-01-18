68f77fac31ce01432760fa425f04b31973aeab0c
julian gaspar <juliangaspar15@gmail.com>
2023-01-18 11:29:13 +0100

    Refactored code, pending to solve the images

--- app/javascript/controllers/drag_home_controller.js
+++ app/javascript/controllers/drag_home_controller.js
@@ -7,4 +7,19 @@ export default class extends Controller {
   connect() {
     this.draggingTarget.scrollIntoView({behavior:"smooth"});
   }
+
+  nav(event) {
+    // REDIRECT TO USER'S PROFILE
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      setTimeout(
+          window.location.replace(this.element.dataset.userPath)
+        , 0)
+      }
+    // REDIRECT TO SEARCH (INDEX)
+    else if ((window.scrollY) === 0) {
+      setTimeout(
+          window.location.replace(this.element.dataset.itemsPath)
+        , 0)
+      }
+    }
 }
